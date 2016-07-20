960b93a3011a54ceb74cbe66e8ed5ddd9b922fed
Marlene Boehmer <s9meboeh@stud.uni-saarland.de>
2016-07-20 14:35:35 +0200

    added proximity filter

--- implementation/epuck/src/proximity-filter.c
+++ implementation/epuck/src/proximity-filter.c
@@ -2,25 +2,32 @@
 #include "proximity-filter.h"
 #include "sensors.h"
 
-static const int THRESHOLD = 4;
-static const double aperature_angle = 0.261799; /* 15 deg */
+#define THRESHOLD 17
+#define APERATURE_ANGLE 15/180*M_PI /* 15 deg */
+
+static void filter_proximity_single(Sensors* sens, unsigned int index, double angle, double victim_phi) {
+    if (fabsf(fmodf(sens->current.phi + angle, 2*M_PI) - victim_phi) < APERATURE_ANGLE) {
+        sens->proximity[index] = 100;
+    }
+}
 
 void filter_proximity(Position victim, Sensors* sens) {
     double delta_x = victim.x - sens->current.x;
     double delta_y = victim.y - sens->current.y;
     double dist = (int) (sqrt(fabs(delta_x * delta_x) + fabs(delta_y * delta_y)));
-    double angle_tb_v;
 
-    if(dist > THRESHOLD) {
+    if (dist > THRESHOLD) {
         return;
     }
 
-    angle_tb_v = atan2f(delta_y, delta_x);
-    angle_tb_v = sens->current.phi - angle_tb_v;
-    if(angle_tb_v < 0) {
-        angle_tb_v += 2 * M_PI;
-    }
-    angle_tb_v -= M_PI;
-
+    double victim_phi = fmodf(atan2f(delta_y, delta_x) + 2*M_PI, 2*M_PI);
+    filter_proximity_single(sens, PROXIMITY_P_20, 20/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_P_45, 45/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_P_90, 90/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_P_150, 150/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_M_150, (360-150)/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_M_90, (360-90)/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_M_45, (360-45)/180*M_PI, victim_phi);
+    filter_proximity_single(sens, PROXIMITY_M_20, (360-20)/180*M_PI, victim_phi);
 
 }
