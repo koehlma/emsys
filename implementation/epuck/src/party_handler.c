03966fca4a05740c655e1bab0d2766f005b70cfd
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-07-26 14:45:51 +0200

    Gave priority to party hard (over party soft).

--- implementation/epuck/src/party_handler.c
+++ implementation/epuck/src/party_handler.c
@@ -13,14 +13,14 @@ void phandler_reset(PHandlerState* state) {
 void phandler_step(PHandlerInput* input, PHandlerState* state) {
     unsigned int led_index;
     int on;
-    if(input->party_soft) {
-        state->hard_state = 0;
-        state->soft_state = (state->soft_state + 1) % SOFT_LED_DIST;
-        set_leds(state->soft_state);
-    } else if(input->party_hard) {
+    if(input->party_hard) { /* priority on party hard */
         state->soft_state = 0;
         state->hard_state = (state->hard_state + 1) % HARD_LED_DIST;
         set_leds(state->hard_state);
+    } else if(input->party_soft) {
+        state->hard_state = 0;
+        state->soft_state = (state->soft_state + 1) % SOFT_LED_DIST;
+        set_leds(state->soft_state);
     }
 }
 
