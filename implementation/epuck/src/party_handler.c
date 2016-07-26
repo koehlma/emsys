6bf8dafd307a587106bf7b7a55d1d2cdd9faa7fe
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-07-26 15:17:13 +0200

    Now using all LEDs; no longer doing crap. Well, at least doing other crap.

--- implementation/epuck/src/party_handler.c
+++ implementation/epuck/src/party_handler.c
@@ -4,7 +4,7 @@
 #define SOFT_LED_DIST (3)
 #define HARD_LED_DIST (2)
 
-void set_leds(int state);
+void set_leds(int state, int dist);
 
 void phandler_reset(PHandlerState* state) {
     state->soft_state = state->hard_state = 0;
@@ -16,19 +16,19 @@ void phandler_step(PHandlerInput* input, PHandlerState* state) {
     if(input->party_hard) { /* priority on party hard */
         state->soft_state = 0;
         state->hard_state = (state->hard_state + 1) % HARD_LED_DIST;
-        set_leds(state->hard_state);
+        set_leds(state->hard_state, HARD_LED_DIST);
     } else if(input->party_soft) {
         state->hard_state = 0;
         state->soft_state = (state->soft_state + 1) % SOFT_LED_DIST;
-        set_leds(state->soft_state);
+        set_leds(state->soft_state, SOFT_LED_DIST);
     }
 }
 
-void set_leds(int state) {
+void set_leds(int state, int dist) {
     unsigned int led_index;
     int on;
-    for(led_index = 1; led_index < 8; ++led_index) {
-        on = (led_index % state == 0);
+    for(led_index = 0; led_index < 8; ++led_index) {
+        on = (led_index % dist == state);
         hal_set_led(led_index, (unsigned int) on);
     }
 }
